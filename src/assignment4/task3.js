9271d7eec554cc09e35da2f9f617ab62efd97d15
Antrix <antriksh@vividtheory.com>
2022-02-25 12:34:27 -0700

    Updated Task3

--- src/assignment4/task3.js
+++ src/assignment4/task3.js
@@ -19,6 +19,8 @@ var isLeaf = function (t) {
 var isBranch = function (t) {
     return t.tag === "branch";
 };
+// const leaf = <T>(val: T): Tree<T> => { return { tag: "leaf", value: val } }
+// const branch = <T>(tree: Tree<T>): Tree<T> => { return { tag: "branch", left: tree, right: tree } }
 // Signature    size: Tree<A> -> number
 var size = function (tree) {
     if (isLeaf(tree)) {
@@ -29,8 +31,13 @@ var size = function (tree) {
     }
 };
 var tree1 = {
-    tag: "leaf",
-    value: "leaf1"
+    tag: "branch",
+    left: { tag: "leaf", value: 8 },
+    right: {
+        tag: "branch",
+        left: { tag: "leaf", value: 5 },
+        right: { tag: "leaf", value: 9 }
+    }
 };
 // console.log(size(tree1)); // 3
 // Signature   max: Tree<number> -> number
@@ -40,11 +47,7 @@ var tree2 = {
     right: {
         tag: "branch",
         left: { tag: "leaf", value: 5 },
-        right: {
-            tag: "branch",
-            left: { tag: "leaf", value: 7 },
-            right: { tag: "leaf", value: 9 }
-        }
+        right: { tag: "leaf", value: 9 }
     }
 };
 var max = function (tree) {
@@ -71,4 +74,92 @@ var depth = function (tree) {
         return depthLeft >= depthRight ? depthLeft : depthRight;
     }
 };
-console.log(depth(tree2));
+var tree3 = {
+    tag: "branch",
+    left: { tag: "leaf", value: 8 },
+    right: {
+        tag: "branch",
+        left: { tag: "leaf", value: 5 },
+        right: {
+            tag: "branch",
+            left: { tag: "leaf", value: 7 },
+            right: { tag: "leaf", value: 9 }
+        }
+    }
+};
+// console.log(depth(tree3)); // 3
+// Signature  map: ((A)-> B) -> Tree<A> -> Tree<B>
+var map = function (f, tree) {
+    if (isLeaf(tree)) {
+        return new Leaf(f(tree.value));
+    }
+    else {
+        return new Branch(map(f, tree.left), map(f, tree.right));
+    }
+};
+// curry form
+var mapCurry = function (f) { return function (tree) {
+    if (isLeaf(tree)) {
+        return new Leaf(f(tree.value));
+    }
+    else {
+        return new Branch(map(f, tree.left), map(f, tree.right));
+    }
+}; };
+var tree4 = {
+    tag: "branch",
+    left: { tag: "leaf", value: 2 },
+    right: {
+        tag: "branch",
+        left: { tag: "leaf", value: 3 },
+        right: { tag: "leaf", value: 4 }
+    }
+};
+// console.log(map((i) => i + 1, tree4)); 
+/**
+ *
+ * Branch { tag: 'branch',
+    left: Leaf { tag: 'leaf', value: 3 },
+    right: Branch {
+        tag: 'branch',
+        left: Leaf { tag: 'leaf', value: 4 },
+        right: Leaf { tag: 'leaf', value: 5 }
+        }
+    }
+ *
+ */
+// console.log(mapCurry((i: number) => i + 1)(tree4));
+/**
+ *
+ * Branch { tag: 'branch',
+    left: Leaf { tag: 'leaf', value: 3 },
+    right: Branch {
+        tag: 'branch',
+        left: Leaf { tag: 'leaf', value: 4 },
+        right: Leaf { tag: 'leaf', value: 5 }
+        }
+    }
+ *
+ */
+// Signature  filter : ((Tree<A>)=> boolean) => Tree<A> => Tree<A>
+var filter = function (f, tree) {
+    if (isLeaf(tree)) {
+        if (isLeaf(tree) === null) {
+            return;
+        }
+        return !f(tree.value) ? new Leaf(tree.value) : null;
+    }
+    else {
+        return new Branch(filter(f, tree.left), filter(f, tree.right));
+    }
+};
+var tree5 = {
+    tag: "branch",
+    left: { tag: "leaf", value: 2 },
+    right: {
+        tag: "branch",
+        left: { tag: "leaf", value: 5 },
+        right: { tag: "leaf", value: 4 }
+    }
+};
+console.log(filter(function (i) { return i === 5; }, tree5));
--- src/assignment4/task3.ts
+++ src/assignment4/task3.ts
@@ -1,101 +1,197 @@
 type Tree<A> = Leaf<A> | Branch<A>;
 
 class Leaf<A> {
-  tag: "leaf" = "leaf";
-  readonly value: A;
+    tag: "leaf" = "leaf";
+    readonly value: A;
 
-  constructor(value: A) {
-    this.value = value;
-  }
+    constructor(value: A) {
+        this.value = value;
+    }
 }
 
 class Branch<A> {
-  tag: "branch" = "branch";
-  readonly left: Tree<A>;
-  readonly right: Tree<A>;
+    tag: "branch" = "branch";
+    readonly left: Tree<A>;
+    readonly right: Tree<A>;
 
-  constructor(left: Tree<A>, right: Tree<A>) {
-    this.left = left;
-    this.right = right;
-  }
+    constructor(left: Tree<A>, right: Tree<A>) {
+        this.left = left;
+        this.right = right;
+    }
 }
 
-const isLeaf = <T>(t: Tree<T>): t is Leaf<T> => {
-  return t.tag === "leaf";
+const isLeaf = <A>(t: Tree<A>): t is Leaf<A> => {
+    return t.tag === "leaf";
 };
-const isBranch = <T>(t: Tree<T>): t is Branch<T> => {
-  return t.tag === "branch";
+const isBranch = <A>(t: Tree<A>): t is Branch<A> => {
+    return t.tag === "branch";
 };
+
+// const leaf = <T>(val: T): Tree<T> => { return { tag: "leaf", value: val } }
+
+// const branch = <T>(tree: Tree<T>): Tree<T> => { return { tag: "branch", left: tree, right: tree } }
 
 // Signature    size: Tree<A> -> number
 const size = <A>(tree: Tree<A>): number => {
-  if (isLeaf(tree)) {
-    return 1;
-  } else {
-    return 1 + size(tree.left) + size(tree.right);
-  }
+    if (isLeaf(tree)) {
+        return 1;
+    } else {
+        return 1 + size(tree.left) + size(tree.right);
+    }
 };
 
 const tree1: Tree<number> = {
-  tag: "branch",
-  left: { tag: "leaf", value: 8 },
-  right: {
     tag: "branch",
-    left: { tag: "leaf", value: 5 },
-    right: { tag: "leaf", value: 9 },
-  },
+    left: { tag: "leaf", value: 8 },
+    right: {
+        tag: "branch",
+        left: { tag: "leaf", value: 5 },
+        right: { tag: "leaf", value: 9 },
+    },
 };
 // console.log(size(tree1)); // 3
 
 // Signature   max: Tree<number> -> number
 
 const tree2: Tree<number> = {
-  tag: "branch",
-  left: { tag: "leaf", value: 8 },
-  right: {
     tag: "branch",
-    left: { tag: "leaf", value: 5 },
-    right: { tag: "leaf", value: 9 },
-  },
+    left: { tag: "leaf", value: 8 },
+    right: {
+        tag: "branch",
+        left: { tag: "leaf", value: 5 },
+        right: { tag: "leaf", value: 9 },
+    },
 };
 
 const max = (tree: Tree<number>): number => {
-  let currentMax = 0;
-  if (isLeaf(tree)) {
-    currentMax = currentMax >= tree.value ? currentMax : tree.value;
-  } else {
-    const leftMax = max(tree.left);
-    const rightMax = max(tree.right);
-    currentMax = leftMax >= rightMax ? leftMax : rightMax;
-  }
-  return currentMax;
+    let currentMax = 0;
+    if (isLeaf(tree)) {
+        currentMax = currentMax >= tree.value ? currentMax : tree.value;
+    } else {
+        const leftMax = max(tree.left);
+        const rightMax = max(tree.right);
+        currentMax = leftMax >= rightMax ? leftMax : rightMax;
+    }
+    return currentMax;
 };
 // console.log(max(tree2)); //9
 
 // Signature  depth: Tree<A> -> number
 
 const depth = <A>(tree: Tree<A>): number => {
-  if (isLeaf(tree)) {
-    return 0;
-  } else {
-    const depthLeft = 1 + depth(tree.left);
-    const depthRight = 1 + depth(tree.right);
-    return depthLeft >= depthRight ? depthLeft : depthRight;
-  }
+    if (isLeaf(tree)) {
+        return 0;
+    } else {
+        const depthLeft = 1 + depth(tree.left);
+        const depthRight = 1 + depth(tree.right);
+        return depthLeft >= depthRight ? depthLeft : depthRight;
+    }
 };
 
 const tree3: Tree<number> = {
-  tag: "branch",
-  left: { tag: "leaf", value: 8 },
-  right: {
     tag: "branch",
-    left: { tag: "leaf", value: 5 },
+    left: { tag: "leaf", value: 8 },
     right: {
-      tag: "branch",
-      left: { tag: "leaf", value: 7 },
-      right: { tag: "leaf", value: 9 },
+        tag: "branch",
+        left: { tag: "leaf", value: 5 },
+        right: {
+            tag: "branch",
+            left: { tag: "leaf", value: 7 },
+            right: { tag: "leaf", value: 9 },
+        },
     },
-  },
 };
 
-console.log(depth(tree3)); // 3
+// console.log(depth(tree3)); // 3
+
+
+// Signature  map: ((A)-> B) -> Tree<A> -> Tree<B>
+
+const map = <A, B>(
+    f: (a: A) => B,
+    tree: Tree<A>): Tree<B> => {
+    if (isLeaf(tree)) {
+        return new Leaf(f(tree.value))
+    } else {
+        return new Branch(map(f, tree.left), map(f, tree.right))
+    }
+}
+
+// curry form
+
+const mapCurry = <A, B>(f: (a: A) => B) => (tree: Tree<A>): Tree<B> => {
+    if (isLeaf(tree)) {
+        return new Leaf(f(tree.value))
+    } else {
+        return new Branch(map(f, tree.left), map(f, tree.right))
+    }
+}
+const tree4: Tree<number> = {
+    tag: "branch",
+    left: { tag: "leaf", value: 2 },
+    right: {
+        tag: "branch",
+        left: { tag: "leaf", value: 3 },
+        right: { tag: "leaf", value: 4 }
+    },
+};
+
+// console.log(map((i) => i + 1, tree4)); 
+
+/**
+ *
+ * Branch { tag: 'branch',
+    left: Leaf { tag: 'leaf', value: 3 },
+    right: Branch {
+        tag: 'branch',
+        left: Leaf { tag: 'leaf', value: 4 },
+        right: Leaf { tag: 'leaf', value: 5 }
+        }
+    }
+ *
+ */
+
+
+// console.log(mapCurry((i: number) => i + 1)(tree4));
+
+/**
+ * 
+ * Branch { tag: 'branch',
+    left: Leaf { tag: 'leaf', value: 3 },
+    right: Branch {
+        tag: 'branch',
+        left: Leaf { tag: 'leaf', value: 4 },
+        right: Leaf { tag: 'leaf', value: 5 }
+        }
+    }
+ * 
+ */
+
+// Signature  filter : ((Tree<A>)=> boolean) => Tree<A> => Tree<A>
+
+const filter = <A>(
+    f: (val: A) => boolean,
+    tree: Tree<A>): any => {
+
+    if (isLeaf(tree)) {
+        return !f(tree.value) ? new Leaf(tree.value) : null
+
+    } else {
+        return new Branch(filter(f, tree.left), filter(f, tree.right))
+
+    }
+
+}
+const tree5: Tree<number> = {
+    tag: "branch",
+    left: { tag: "leaf", value: 2 },
+    right: {
+        tag: "branch",
+        left: { tag: "leaf", value: 5 },
+        right: { tag: "leaf", value: 4 }
+    },
+};
+
+console.log(filter((i) => i === 5, tree5))
+
+
